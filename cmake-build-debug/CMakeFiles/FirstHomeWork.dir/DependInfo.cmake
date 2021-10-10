41167f202478ffb1ebf0ff2e7d37d8ec77add35d
Nikita Zubarev <zubarenik@gmail.com>
2021-10-10 22:22:51 +0300

    add cmake

---
+++ cmake-build-debug/CMakeFiles/FirstHomeWork.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/container.c" "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/cmake-build-debug/CMakeFiles/FirstHomeWork.dir/container.c.o"
+  "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/figure.c" "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/cmake-build-debug/CMakeFiles/FirstHomeWork.dir/figure.c.o"
+  "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/main.c" "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/cmake-build-debug/CMakeFiles/FirstHomeWork.dir/main.c.o"
+  "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/parallelepiped.c" "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/cmake-build-debug/CMakeFiles/FirstHomeWork.dir/parallelepiped.c.o"
+  "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/random.c" "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/cmake-build-debug/CMakeFiles/FirstHomeWork.dir/random.c.o"
+  "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/sphere.c" "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/cmake-build-debug/CMakeFiles/FirstHomeWork.dir/sphere.c.o"
+  "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/tetrahedron.c" "/home/nikita/CLionProjects/FirstHomeWork/CA-FirstHomework/cmake-build-debug/CMakeFiles/FirstHomeWork.dir/tetrahedron.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FirstHomeWork.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstHomeWork.dir/container.c.o"
+  "CMakeFiles/FirstHomeWork.dir/figure.c.o"
+  "CMakeFiles/FirstHomeWork.dir/main.c.o"
+  "CMakeFiles/FirstHomeWork.dir/parallelepiped.c.o"
+  "CMakeFiles/FirstHomeWork.dir/random.c.o"
+  "CMakeFiles/FirstHomeWork.dir/sphere.c.o"
+  "CMakeFiles/FirstHomeWork.dir/tetrahedron.c.o"
+  "FirstHomeWork"
+  "FirstHomeWork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FirstHomeWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
